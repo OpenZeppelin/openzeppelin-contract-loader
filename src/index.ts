d7b625e6dc0790b222b3f2a2a8b1b9a1eb4a4a9c
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-09-12 18:53:22 -0300

    Search using find-up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import web3Contract from 'web3/eth/contract';
-import fs from 'fs';
+import fs from 'fs-extra';
+import findUp from 'find-up';
 
-export default function(contract: string) {
-  const { abi, bytecode } = JSON.parse(fs.readFileSync(`./build/contracts/${contract}.json`, 'utf8'));
+export default async function(contract: string) {
+  const artifactsDir = await findUp('build/contracts');
+  if (!artifactsDir) {
+    throw new Error('Could not find compiled artifacts directory');
+  }
+
+  const { abi, bytecode } = await fs.readJSON(`${artifactsDir}/${contract}.json`, { encoding: 'utf8' });
   return new web3Contract(abi, undefined, { data: bytecode });
 }
